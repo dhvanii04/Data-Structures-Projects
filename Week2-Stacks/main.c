9dfcfbd07bdf883486537815c0fde7290bbb9d30
Tanish Patel <tanishpatel0106@gmail.com>
2022-09-09 10:06:45 +0530

    Update main.c

--- Week2-Stacks/main.c
+++ Week2-Stacks/main.c
@@ -63,15 +63,19 @@ int main(){
         }
     }
 
-    fileWrite(date, transactionID, debit, credit, top_date);
+    fileWrite(date, transactionID, debit, credit, top_date, balance);
+    fileWritetxt(date, transactionID, debit, credit, top_date, acc_number, balance);
 
     printf("\nAccount Number: %d \n", acc_number);
     printf("Opening Balance: %d \n", balance);
-    printf("\nDate\t\tTransaction ID\t\tDebit\t\tCredit\t\tBalance \n");
+    printf("+------------------------------------------------------------------------------+");
+    printf("|\nDate\t\tTransaction ID\t\tDebit\t\tCredit\t\tBalance \n|");
+    printf("+------------------------------------------------------------------------------+");
     for(int i = 0; i < top_date; i++){
         balance = balance - debit[i] + credit[i];
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d \n", date[i], transactionID[i], debit[i], credit[i], balance);
+        printf("|%d\t\t%d\t\t%d\t\t%d\t\t%d\n|", date[i], transactionID[i], debit[i], credit[i], balance);
     }
+    printf("+------------------------------------------------------------------------------+");
     for(int i=0; i<top_date; i++){
         pop(date, top_date);
     }
