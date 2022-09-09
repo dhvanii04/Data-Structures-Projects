9774f4e07b56d4dacaee008a3400173626b41e54
Tanish Patel <tanishpatel0106@gmail.com>
2022-09-09 10:07:09 +0530

    Update fileIO.h

--- Week2-Stacks/fileIO.h
+++ Week2-Stacks/fileIO.h
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-void fileWrite(int date[100], int transactionID[100], int debit[100], int credit[100], int top_date){
+void fileWrite(int date[100], int transactionID[100], int debit[100], int credit[100], int top_date, int balance){
     FILE *fp;
     fp = fopen("file.csv", "w");
     fprintf(fp, "Date, Transaction ID, Debit, Credit, Balance \n");
-    int balance = 0;
     for(int i = 0; i < top_date; i++){
         balance = balance - debit[i] + credit[i];
         fprintf(fp, "%d,%d,%d,%d,%d\n", date[i], transactionID[i], debit[i], credit[i], balance);
     }
+}
+
+void fileWritetxt(int date[100], int transactionID[100], int debit[100], int credit[100], int top_date, int acc_number, int balance){
+    FILE *fptr;
+    fptr = fopen("file.txt", "w");
+    fprintf(fptr, "\nAccount Number: %d \n", acc_number);
+    fprintf(fptr, "Opening Balance: %d \n", balance);
+    fprintf(fptr, "+------------------------------------------------------------------------------+\n");
+    fprintf(fptr, "|\nDate\t\tTransaction ID\t\tDebit\t\tCredit\t\tBalance \n|");
+    fprintf(fptr, "+------------------------------------------------------------------------------+\n");
+    for(int i = 0; i < top_date; i++){
+        balance = balance - debit[i] + credit[i];
+        fprintf(fptr, "|%d\t\t%d\t\t%d\t\t%d\t\t%d\n|", date[i], transactionID[i], debit[i], credit[i], balance);
+    }
+    fprintf(fptr, "+------------------------------------------------------------------------------+\n");
 }
