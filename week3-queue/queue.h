a5c7ba463ad97baff3288d30fe9ff1adc5c818d5
Datt Dixit <94307881+DattDixit@users.noreply.github.com>
2022-09-15 16:41:01 +0530

    Update queue.h

--- week3-queue/queue.h
+++ week3-queue/queue.h
@@ -1,48 +1,21 @@
 #include<stdio.h>
 //queue library
-int capacity;
-  int a[capacity];
-    int front=0;
-    int rear=0;
-    int choice;
-void enqueue(int x)
-{
-    if(rear==capacity-1)
-    {
-        printf("Queue is full");
-    }
-    else
-    {
-        a[rear]=x;
-        rear++;
-    }
+  
+int enqueue(int a[100], int data, int rear)
+{  
+    a[rear] = data;
+    rear++;
+    return a[rear-1], rear;
 }
-
-void dequeue()
+int dequeue(int a[100], int front)
 {
-   if (front==0 || front>rear)
-   {
-       printf("Queue is empty");
-   }
-   else
-   {
-       front++;
-   if(front>rear)
-   {
-       front=rear=0;
-   }
-   }
-
-void display()
-{ if(rear==0)
-    {
+    if(front==-1){
         printf("Queue is empty");
     }
-    else
-    {
-        for(int i=front;i<rear;i++)
-        {
-            printf("%d ",a[i]);
-        }
+    else{
+        a[front] = 0;
+        front++;
+        return a[100], front;
     }
 }
+     
