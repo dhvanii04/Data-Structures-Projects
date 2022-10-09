8e3bf3a0464190881e42f378089567dfd75c72ff
karanx26 <112680487+karanx26@users.noreply.github.com>
2022-10-10 00:40:15 +0530

    Update main.c

--- week3-queue/main.c
+++ week3-queue/main.c
@@ -1,51 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "queue.h"
-int main(){
-    printf("+---------------------------------------------------+\n");
-    printf("|                CodeForces Ranking System          |\n");
-    printf("+---------------------------------------------------+\n");
-int capacity;
+ 
+int front, rear;
+ 
+int main()
+{
+    int s;
 
-int front=0;
-    int rear=0;
-    char name[20];
-    int id;
-int a[100];
-int b[100];
-int c[100];
-int i;
-int users=3;
+    printf("+-------------------------------------------------+\n");
+    printf("|                     DSA Quiz                    |\n");
+    printf("+-------------------------------------------------+\n");
+    
+    printf("\nHow many Candidates are there for quiz : ");
+    scanf("%d",&s);
 
+    int c[s];
+    char name[10];
+    int ans;
+    int score=0;
+    int pri_que[s];
+    float avg;
 
-while(users--){   //question 1 inputs
-    printf("For submitting question 1A,please enter your id");
-    scanf("%d",&id);
-    a[100],rear=enqueue(a, id, rear);
+    create(); 
+
+    for(int i=0;i<s;i++)
+    {
+        c[i]=0;
+        printf("\n+-------------------------------------------------+\n");
+        printf("\n%d. Enter the name : ",i+1);
+        scanf("%s",name);
+        
+        printf("\nAnswer the following True/False. \n(1 for True, 0 for False)\n(Maximum marks = 3)\n");
+        
+        printf("\nQ1. AB+C-D/ is the Postfix expression of (A+B-C)/D : ");
+        scanf("%d",&ans);
+        if(ans==1)
+        c[i]++;
+        
+        printf("\nQ2. Stack works on FIFO principle : ");
+        scanf("%d",&ans);
+        if(ans==0)
+        c[i]++;
+        
+        printf("\nQ3. If top=0 then Stack is empty : ");
+        scanf("%d",&ans);
+        if(ans==0)
+        c[i]++;
+        
+        printf("\n||Final Score of %s is : %d|| \n",name,c[i]);
+        score=c[i];
+        
+        enqueue(score);
     }
-    users=3;
-while(users--){ //question 2 inputs
-printf("For submitting question 1B,please enter your id");
-scanf("%d",&id);
-b[100],rear=enqueue(b, id, rear);
-}
-users=3;
-while(users--){ //question 3 inputs
-printf("For submitting question 1C,please enter your id");
-scanf("%d",&id);
-c[100],rear=enqueue(c, id, rear);
 
-}
-Finalranks(a,b,c,users);
-}
-void Finalranks(int a[],int b[], int c[],int users){
-    printf("-----------------------------");
-    printf("Rating for questions are:");
-    printf("|\nQuestion 1A\t\tQuestion 1B\t\tQuestion 3\n|");
-    for(int i=0;i<users;i++){
-        printf("|%d\t\t%d\t\t%d|\n",a[100],b[100],c[100]);
-    }
-    printf("---------------------------------------------");
-}
+    printf("\n+-------------------------------------------------+\n");
 
+    printf("\nFinal Scores of Candidates from Highest to Lowest : \n");
+    display();
+
+    avg=sum/s;
+    printf("\nAverage Score of Candidates is : %f\n",avg); 
+}
+ 
