f26656a38c1db1b5344dd7d624e9d4e7bfa7e4ce
karanx26 <112680487+karanx26@users.noreply.github.com>
2022-10-10 00:40:52 +0530

    Update queue.h

--- week3-queue/queue.h
+++ week3-queue/queue.h
@@ -1,21 +1,63 @@
 #include<stdio.h>
-//queue library
+
+// priority queue library
+void create(); 
+void enqueue(int data);
+void check(int data);
+void display();
+ 
+int pri_que[100];
+int front, rear;
+float sum;
   
-int enqueue(int a[100], int data, int rear)
-{  
-    a[rear] = data;
-    rear++;
-    return a[rear-1], rear;
-}
-int dequeue(int a[100], int front)
+// Function to create an empty priority queue
+void create()
 {
-    if(front==-1){
-        printf("Queue is empty");
-    }
-    else{
-        a[front] = 0;
-        front++;
-        return front;
-    }
+    front = rear = -1;
 }
-     
+ 
+// Function to insert value into priority queue
+void enqueue(int data)
+{
+    if ((front == -1) && (rear == -1))
+    {
+        front++;
+        rear++;
+        pri_que[rear] = data;
+        return;
+    }    
+    else
+        check(data);
+    rear++;
+}
+ 
+// Function to check priority and place element 
+void check(int data)
+{
+    int i,j;
+ 
+    for (i = 0; i <= rear; i++)
+    {
+        if (data >= pri_que[i])
+        {
+            for (j = rear + 1; j > i; j--)
+            {
+                pri_que[j] = pri_que[j - 1];
+            }
+            pri_que[i] = data;
+            return;
+        }
+    }
+    pri_que[i] = data;
+}
+ 
+// Function to display queue elements 
+void display()
+{
+    for (; front <= rear; front++)
+    {
+        printf(" %d\n", pri_que[front]);
+        sum=sum+pri_que[front];
+    }
+    front = 0;
+}
